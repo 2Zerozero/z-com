d1cf471b06d91ade1b6f0e47151cebd60f247b59
2Zerozero <hawgae@gmail.com>
2023-12-11 13:28:40 +0900

    [feat] 레이아웃 타입 지정

---
+++ src/app/(beforeLogin)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+export default function Layout({ children, modal }: { children: ReactNode; modal: ReactNode }) {
+  return (
+    <div>
+      비포 로그인 레이아웃
+      {children}
+      {modal}
+    </div>
+  );
+}
