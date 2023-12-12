984543d50749ad902e1876a28dffcde17d33d19c
2Zerozero <hawgae@gmail.com>
2023-12-12 19:41:16 +0900

    [feat] 컴포넌트 분리 및 수정

---
+++ src/app/(beforeLogin)/@modal/(.)i/flow/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginModal from '@/app/(beforeLogin)/_component/LoginModal';
+
+export default function Page() {
+  return <LoginModal />;
+}
+
+// router.push
+// localhost:3000 -> localhost:3000/login -> localhost:3000/i/flow/login
+
+// router.replace
+// localhost:3000 -> localhost:3000/login -? localhost:3000/i/flow/login
