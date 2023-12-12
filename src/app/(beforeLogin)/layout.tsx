38ef3ea2df937bfce2c7c3091d8bc3e5d7584ad9
2Zerozero <hawgae@gmail.com>
2023-12-12 10:35:34 +0900

    [feat] 레이아웃 주석 제거

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from 'react';
+import stlyes from '@/app/page.module.css';
 
-export default function Layout({ children, modal }: { children: ReactNode; modal: ReactNode }) {
+type Props = { children: ReactNode; modal: ReactNode };
+
+export default function Layout({ children, modal }: Props) {
   return (
-    <div>
-      비포 로그인 레이아웃
+    <div className={stlyes.container}>
       {children}
       {modal}
     </div>
   );
 }
+
+// 주소가 z.com 일때
+// children => children => page.tsx
+// modal => @modal/default.tsx
