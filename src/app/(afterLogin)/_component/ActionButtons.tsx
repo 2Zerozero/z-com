e756261a42f9aeff370c7139c7b725ea3d885391
2Zerozero <hawgae@gmail.com>
2023-12-15 22:27:28 +0900

    [feat] 액션 버튼 구현시작

---
+++ src/app/(afterLogin)/_component/ActionButtons.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import styles from '@/app/(afterLogin)/_component/actionButtons.module.css';
+
+export default function ActionButtons() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
