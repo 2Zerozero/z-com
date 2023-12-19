102519af53384233200e25b0a67555b78016e95f
2Zerozero <hawgae@gmail.com>
2023-12-19 14:59:02 +0900

    [feat] Explore / Search 구현

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,26 @@
+import styles from '@/app/(afterLogin)/explore/explore.module.css';
+import SearchForm from '../_component/SearchForm';
+import Trend from '../_component/Trend';
+
 export default function Home() {
-  return <main>검색 페이지</main>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.formZone}>
+        <SearchForm />
+      </div>
+      <div className={styles.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
