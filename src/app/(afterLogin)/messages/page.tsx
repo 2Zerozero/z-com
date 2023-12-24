58cfdff165374898c4a581780cd9a1a914c929fe
2Zerozero <hawgae@gmail.com>
2023-12-24 15:14:10 +0900

    [feat] 메세지 / 세부 채팅 페이지 구현

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,18 @@
+import styles from '@/app/(afterLogin)/messages/message.module.css';
+import Room from './_component/Room';
+
 export default function Home() {
-  return <main>쪽지 페이지</main>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
